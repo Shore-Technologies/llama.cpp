bfcf7c5dd0ecd0dc6b37b0532b97ab631264482f
Invisi <invisiofficial@gmail.com>
2025-10-17 04:10:09 -0400

    RKNPU2 backend is implemented

--- ggml/src/ggml-backend-reg.cpp
+++ ggml/src/ggml-backend-reg.cpp
@@ -78,6 +78,19 @@
 #include "ggml-cann.h"
 #endif
 
+#ifdef GGML_USE_RKNPU2
+#include "ggml-rknpu2.h"
+#endif
+
+// disable C++17 deprecation warning for std::codecvt_utf8
+#if defined(__clang__)
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(__GNUC__)
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 #ifdef GGML_USE_ZENDNN
 #include "ggml-zendnn.h"
 #endif
@@ -163,6 +176,9 @@ struct ggml_backend_registry {
 #endif
 #ifdef GGML_USE_CPU
         register_backend(ggml_backend_cpu_reg());
+#endif
+#ifdef GGML_USE_RKNPU2
+        register_backend(ggml_backend_rknpu2_reg());
 #endif
     }
 
